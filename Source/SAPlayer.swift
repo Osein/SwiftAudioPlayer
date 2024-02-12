4004307b504236eebc2d82d8b751f50a2534d858
Osein <huseyin.gucuyener@gmail.com>
2024-02-12 22:35:23 +0300

    Added duck others to AVAudioSession category

--- Source/SAPlayer.swift
+++ Source/SAPlayer.swift
@@ -577,7 +577,7 @@ extension SAPlayer: SAPlayerDelegate {
     private func becomeDeviceAudioPlayer() {
         do {
             if #available(iOS 11.0, tvOS 11.0, *) {
-                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .spokenAudio, policy: .longFormAudio, options: [])
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .spokenAudio, policy: .longFormAudio, options: [.duckOthers])
             } else {
                 try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode(rawValue: convertFromAVAudioSessionMode(AVAudioSession.Mode.default)), options: .allowAirPlay)
             }
